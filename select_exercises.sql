a7092056a86ae57780018222382da21302e703c0
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-15 09:55:04 -0500

    edited captions

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,28 +1,28 @@
 USE codeup_test_db;
 # Write queries to find the following information. Before each item, output a caption explaining the results:
 # The name of all albums by Pink Floyd.
-SELECT '' AS 'Pink Floyd Albums';
+SELECT 'Pink Floyd' AS 'Albums By';
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
 # The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT '' AS 'Release year for Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'Sgt. Pepper''s Lonely Hearts Club Band' AS 'Release Year For';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 # The genre for Nevermind
-SELECT '' AS 'Genre of Nevermind';
+SELECT 'Nevermind' AS 'Genre of';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
 # Which albums were released in the 1990s
-SELECT '' AS 'Albums released in the 1990s';
+SELECT '1990s' AS 'Albums released in the';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 # Which albums had less than 20 million certified sales
-SELECT '' AS 'Albums with less than 30 million sales';
+SELECT '30 million sales' AS 'Albums with less than';
 SELECT * FROM albums WHERE sales > 30000000;
 
 # All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 # Use 'like' instead of genre = 'rock', otherwise you will get albums that only have 'rock' as a genre (exact match).
-SELECT '' AS 'Rock albums';
+SELECT 'Rock' AS 'Albums with the genre of';
 SELECT * FROM albums WHERE genre like '%rock%';
 
 -- if already in mysql client use: "source albums_seeder.sql" to run script
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,2 +1,17 @@
 USE codeup_test_db;
+/*
+Write SELECT statements to output each of the following with a caption:
+
+    All albums in your table.
+    All albums released before 1980
+    All albums by Michael Jackson
+
+    After each SELECT add an UPDATE statement to:
+
+    Make all the albums 10 times more popular (sales * 10)
+    Move all the albums before 1980 back to the 1800s.
+    Change 'Michael Jackson' to 'Peter Jackson'
+
+    Add SELECT statements after each UPDATE so you can see the results of your handiwork.
+*/
 
