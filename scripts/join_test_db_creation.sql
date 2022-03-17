9b0a95f1ba05875c8cadf37e7a05ab2dfdf588cb
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-17 11:25:47 -0500

    added COUNT query, finished exercise

--- scripts/join_test_db_creation.sql
+++ scripts/join_test_db_creation.sql
@@ -1,10 +1,9 @@
 USE join_test_db;
-/*
-    Create a database named join_test_db and run the SQL provided in the Join Example DB section above; to create the same setup used for this lesson.
-    Insert 4 new users into the database. One should have a NULL role. The other three should be authors.
-    Use JOIN, LEFT JOIN, and RIGHT JOIN to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
-    Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use COUNT and the appropriate join type to get a list of roles along with the number of users that have a given role. Hint: You will also need to use GROUP BY in the query.
-*/
+
+# Create a database named join_test_db and run the SQL provided in the Join Example DB section above; to create the same setup used for this lesson.
+-- TRUNCATE each time script is run, to ensure no duplicate tables are created
+TRUNCATE roles;
+TRUNCATE users;
 CREATE TABLE roles
 (
     id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -37,4 +36,38 @@ VALUES ('bob', 'bob@example.com', 1),
        ('sally', 'sally@example.com', 3),
        ('adam', 'adam@example.com', 3),
        ('jane', 'jane@example.com', null),
-       ('mike', 'mike@example.com', null);
+       ('mike', 'mike@example.com', null),
+# Insert 4 new users into the database. One should have a NULL role. The other three should be authors.
+       ('matt', 'matt@example.com', 2),
+       ('stew', 'stew@example.com', 2),
+       ('ray', 'ray@example.com', 2),
+       ('karen', 'karen@example.com', null);
+
+# Use JOIN, LEFT JOIN, and RIGHT JOIN to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
+-- JOIN: Displays all the info that overlaps from both tables (i.e. no NULL values)
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id;
+-- LEFT JOIN: Displays all the info from the LEFT table (users) whether or not there is a role associated
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
+-- RIGHT JOIN: Displays all of the info from the RIGHT table (roles) whether or not there is a user for each role.
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         RIGHT JOIN roles ON users.role_id = roles.id;
+
+# Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use COUNT and the appropriate join type to get a list of roles along with the number of users that have a given role. Hint: You will also need to use GROUP BY in the query.
+SELECT roles.name, COUNT(role_id)
+from users
+         JOIN roles ON users.role_id = roles.id
+GROUP BY roles.name;
+
+# +----------+----------------+
+# | name     | COUNT(role_id) |
+# +----------+----------------+
+# | admin    |              1 |
+# | author   |              4 |
+# | reviewer |              2 |
+# +----------+----------------+
+# 3 rows in set (0.00 sec)
