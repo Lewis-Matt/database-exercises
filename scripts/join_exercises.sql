955866729ab80122c12f247a316bc4fb26696594
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-17 10:25:40 -0500

    created JOIN exercises

---
+++ scripts/join_exercises.sql
@@ -0,0 +1,10 @@
+USE employees;
+# Write a query that shows each department along with the name of the current manager for that department.
+
+# Find the name of all departments currently managed by women.
+
+# Find the current titles of employees currently working in the Customer Service department.
+
+# Find the current salary of all current managers.
+
+# Find the names of all current employees, their department name, and their current manager's name .
